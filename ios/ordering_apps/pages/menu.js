b3837b35e6e92c417823f8d8d00b31a0f1c524f1
Omar Ali <O.aly@getzeal.io>
2025-11-13 15:51:33 +0200

    Refactoring

--- ios/ordering_apps/pages/menu.js
+++ ios/ordering_apps/pages/menu.js
@@ -1,7 +1,16 @@
 output.menu = {
-    category: 'Test Category',
+    gocrispy_category: 'MEALS',
+    gocrispy_pickupitem: 'NUGGETS SMALL',
+    gocrispy_deliveryitem: 'NUGGETS SMALL',
+    takosan_category: 'Test Category',
+    takosan_pickupitem: 'Test Item',
+    takosan_deliveryitem: 'Test Item',
+    dancinggoat_category: 'Test Category',
+    dancinggoat_pickupitem: 'Test Item',
+    dancinggoat_deliveryitem: 'Test Item',
+    coffeeberry_category: 'Test Category',
+    coffeeberry_pickupitem: 'Test Item',
+    coffeeberry_deliveryitem: 'Test Item',
     viewbasket: "view basket",
-    pickupitem: 'Test Item',
-    deliveryitem: 'Test Item',
     addtobasketbtn: 'addto basket'
 }
